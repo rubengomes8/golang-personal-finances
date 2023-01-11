79af81ef367308ac1238dbc204042de2df761581
rsantosgomes <rsantosgomes@ext.worten.pt>
2023-01-11 22:33:17 +0000

    added more categories and all subcategories of expenses, created makefile

--- docker/db/data/1001-expense-categories.sql
+++ docker/db/data/1001-expense-categories.sql
@@ -2,3 +2,5 @@ INSERT INTO expense_categories (name) values ('House');
 INSERT INTO expense_categories (name) values ('Health');
 INSERT INTO expense_categories (name) values ('Transportation');
 INSERT INTO expense_categories (name) values ('Laser');
+INSERT INTO expense_categories (name) values ('Investments');
+INSERT INTO expense_categories (name) values ('Personal');
