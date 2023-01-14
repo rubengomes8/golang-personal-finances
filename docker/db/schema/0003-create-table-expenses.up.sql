4d6cd31a293d8beb1f8637092a63230b2c3f791b
rsantosgomes <rsantosgomes@ext.worten.pt>
2023-01-14 17:28:04 +0000

    added not nulls+

--- docker/db/schema/0003-create-table-expenses.up.sql
+++ docker/db/schema/0003-create-table-expenses.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE expense_categories (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20)
+    name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE expense_subcategories (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20),
-    category_id INTEGER,
+    name VARCHAR(20) NOT NULL,
+    category_id INTEGER NOT NULL,
 
     CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES expense_categories(id)
 );
@@ -17,11 +17,9 @@ CREATE TABLE expenses (
     date DATE NOT NULL,
     description VARCHAR(50),
 
-    category_id INTEGER NOT NULL,
     subcategory_id INTEGER NOT NULL,
     card_id INTEGER NOT NULL,
 
-    CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES expense_categories(id),
     CONSTRAINT fk_subcategory FOREIGN KEY(subcategory_id) REFERENCES expense_subcategories(id),
     CONSTRAINT fk_card FOREIGN KEY(card_id) REFERENCES cards(id)
 );
