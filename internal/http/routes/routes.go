7ac324b8fcabfc34e01a113edd858ae643a59682
rsantosgomes <rsantosgomes@ext.worten.pt>
2023-01-19 10:17:58 +0000

    implemented controller get expenses by dates

--- internal/http/routes/routes.go
+++ internal/http/routes/routes.go
@@ -10,6 +10,10 @@ func SetupRouter(expensesController controllers.ExpensesController) *gin.Engine 
 	v1 := r.Group("/v1")
 	{
 		v1.GET("expense/:id", expensesController.GetExpenseById)
+		v1.GET("expenses/dates/:min_date/:max_date", expensesController.GetExpensesByDates)
+		v1.GET("expenses/category/:category", expensesController.GetExpensesByCategory)
+		v1.GET("expenses/subcategory/:sub_category", expensesController.GetExpensesBySubCategory)
+		v1.GET("expenses/card/:card", expensesController.GetExpensesByCard)
 		v1.POST("expense", expensesController.CreateExpense)
 		v1.PUT("expense/:id", expensesController.UpdateExpense)
 		v1.DELETE("expense/:id", expensesController.DeleteExpense)
